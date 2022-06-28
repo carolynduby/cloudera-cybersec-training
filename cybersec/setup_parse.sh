ebaaa6ab03f58cc89121a489a1b09997794d8e38
cduby <carolynduby@ieee.org>
2022-06-28 14:17:18 -0400

    update parser scripts

--- cybersec/setup_parse.sh
+++ cybersec/setup_parse.sh
@@ -1,3 +1,7 @@
 # create the directory to hold the original source
-hdfs dfs -mkdir -p hdfs:/user/training/cybersec/data/original-source
+hdfs dfs -mkdir -p /user/training/cybersec/data/original-source
+
+# create the patterns directory for metron parsers
+hdfs dfs -mkdir -p /user/training/cybersec/patterns
+hdfs dfs -put configs/parser/metron/patterns/squid /user/training/cybersec/patterns
 
