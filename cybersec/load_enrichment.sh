541501c8e62feb8798f478658b9e435b9e846606
cduby <carolynduby@ieee.org>
2022-06-29 17:25:32 -0400

    fix jar path

--- cybersec/load_enrichment.sh
+++ cybersec/load_enrichment.sh
@@ -9,4 +9,4 @@ fi
 flink run \
 --jobmanager yarn-cluster -yjm 2048 -ytm 2048 --detached --yarnname "Majestic Million Enrichment Import" \
 -yt samples \
-flink-enrichment-loading-2.3.0.jar  $1.properties
+~/jars/flink-enrichment-loading-2.3.0.jar  $1.properties
