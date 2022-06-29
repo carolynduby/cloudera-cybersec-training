724470ed2a9412b265ed3815c65de35530c58697
cduby <carolynduby@ieee.org>
2022-06-29 18:49:09 -0400

    fix triage properties file location

--- cybersec/start_triage.sh
+++ cybersec/start_triage.sh
@@ -6,4 +6,4 @@ fi
 flink run --jobmanager yarn-cluster -yjm 3072 -ytm 3072 \
  --detached --yarnname "edu.triage" \
  $savepoint -yt configs/triage/metron/enrichments \
- ~/jars/flink-enrichment-combined-2.3.0.jar ./configs/triage/triage.propertiess
+ ~/jars/flink-enrichment-combined-2.3.0.jar ./configs/triage/triage.properties
