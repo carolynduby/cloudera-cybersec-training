c963e488a13d2ec2e13c1a7d362c08a55ec9f518
cduby <carolynduby@ieee.org>
2022-06-29 17:18:37 -0400

    add load enrichments

---
+++ cybersec/load_enrichment.sh
@@ -0,0 +1,12 @@
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+    echo "usage: load_enrichment.sh [majestic_million|malicious_domain]"
+    exit 0
+elif [ $1 != "malicious_domain" ] && [ $1 != "majestic_million" ]; then
+    echo "ERROR: unsupported enrichment type $1.  Supported types are malicious_domain and majestic_million"
+    exit 0
+fi
+
+flink run \
+--jobmanager yarn-cluster -yjm 2048 -ytm 2048 --detached --yarnname "Majestic Million Enrichment Import" \
+-yt samples \
+flink-enrichment-loading-2.3.0.jar  $1.properties
