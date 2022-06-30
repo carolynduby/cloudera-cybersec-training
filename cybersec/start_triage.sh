76f7a1afb440a6fff53c3733891f0f4d675eda44
training <training@localhost.localdomain>
2022-06-29 18:42:32 -0700

    fix exec priv

--- cybersec/start_triage.sh
+++ cybersec/start_triage.sh
@@ -3,7 +3,7 @@ if [ $# -eq 1 ]; then
     savepoint=" -s $1"
 fi
 
-flink run --jobmanager yarn-cluster -yjm 3072 -ytm 3072 \
+flink run --jobmanager yarn-cluster -yjm 2048 -ytm 2048 \
  --detached --yarnname "edu.triage" \
  $savepoint -yt configs/triage/metron/enrichments \
  ~/jars/flink-enrichment-combined-2.3.0.jar ./configs/triage/triage.properties
