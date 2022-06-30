33ad810e62a1c894b08801358234f94d751cd3cd
cduby <carolynduby@ieee.org>
2022-06-30 06:36:09 -0400

    profile scripts

---
+++ cybersec/start_profile.sh
@@ -0,0 +1,6 @@
+!/bin/sh
+PHOENIX_CLIENT_JAR=$(ls /opt/cloudera/parcels/CDH/lib/phoenix_queryserver/phoenix-queryserver-client-*.jar -1)
+
+flink run --jobmanager yarn-cluster -yjm 2048 -ytm 2048 --detached --yarnname "edu.profile" \
+--classpath file://${PHOENIX_CLIENT_JAR} \
+~/jars/flink-profiler-java-2.3.0.jar profiler.properties
